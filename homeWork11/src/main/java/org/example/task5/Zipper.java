7a936cb4e08f66ad7051b08efb5bc04a6096c1f0
Martin Dumas <dumasmartin9@gmail.com>
2024-07-13 23:00:47 +0300

    fixed task5

--- homeWork11/src/main/java/org/example/task5/Zipper.java
+++ homeWork11/src/main/java/org/example/task5/Zipper.java
@@ -1,42 +1,25 @@
 package org.example.task5;
 
-import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.Spliterators;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
+
 
 public class Zipper {
-    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second){
-        Iterator<T> firstIterator = first.iterator();
-        Iterator<T> secondIterator = second.iterator();
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        List<T> firstList = first.collect(Collectors.toList());
+        List<T> secondList = second.collect(Collectors.toList());
 
-        Iterator<T> zippedIterator = new Iterator<T>() {
-            private boolean isFirst = true;
-            @Override
-            public boolean hasNext() {
-                return firstIterator.hasNext() && secondIterator.hasNext();
-            }
+        int size = Math.min(firstList.size(), secondList.size());
 
-            @Override
-            public T next() {
-                if (isFirst){
-                    isFirst = false;
-                    return firstIterator.next();
-                } else {
-                    isFirst =true;
-                    return secondIterator.next();
-                }
-            }
-        };
-        Spliterator<T> spliterator = Spliterators.spliteratorUnknownSize(zippedIterator,Spliterator.ORDERED);
-        return StreamSupport.stream(spliterator, false);
-
+        return IntStream.range(0, size)
+                .mapToObj(i -> Stream.of(firstList.get(i), secondList.get(i)))
+                .flatMap(s -> s);
     }
 
     public static void main(String[] args) {
-        Stream<String> first = Stream.of("1", "3", "5", "7", "9");
+        Stream<String> first = Stream.of("1", "3", "5", "A", "7", "8");
         Stream<String> second = Stream.of("2", "4", "6", "8");
         Stream<String> result = zip(first, second);
 
